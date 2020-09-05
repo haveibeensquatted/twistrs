6c95cdb990fee887466ae3ab20fe3e616093041b
Juxhin Dyrmishi Brigjaj <juxhinbox@gmail.com>
2020-09-05 15:08:45 +0200

    Update CLI Example
    - Re-add Clap
    - Re-add colored
    - Update some of the presentation to be more friendly

--- examples/twistrs-cli/src/main.rs
+++ examples/twistrs-cli/src/main.rs
@@ -1,26 +1,43 @@
-use tokio::sync::mpsc;
-
-use std::collections::HashSet;
+use colored::*;
+use clap::{App, Arg};
 
 use twistrs::enrich::DomainMetadata;
 use twistrs::permutate::Domain;
+use tokio::sync::mpsc;
+
+use std::time::Instant;
+use std::collections::HashSet;
 
 
 #[tokio::main]
 async fn main() {
-    let domain = Domain::new("gig.com").unwrap();
+    let start_time = Instant::now();
 
-    let mut _permutations = domain.all().unwrap().collect::<HashSet<String>>();
-    _permutations.insert(String::from(domain.fqdn.clone()));
+    let matches = App::new("twistrs-cli")
+        .version("0.1.0")
+        .author("Juxhin D. Brigjaj <juxhin@phishdeck.com>")
+        .arg(
+            Arg::new("domain")
+                .about("domain to permutate and enrich")
+                .required(true),
+        )
+        .get_matches();
+
+    let domain = Domain::new(&matches.value_of("domain").unwrap()).unwrap();
+
+    let mut domain_permutations = domain.all().unwrap().collect::<HashSet<String>>();
+    let domain_permutation_count = domain_permutations.len();
+
+    domain_permutations.insert(String::from(domain.fqdn.clone()));
 
     let (tx, mut rx) = mpsc::channel(5000);
 
-    for (i, v) in _permutations.into_iter().enumerate() {
+    for (i, v) in domain_permutations.into_iter().enumerate() {
         let domain_metadata = DomainMetadata::new(v.clone());
         let mut tx = tx.clone();
 
         tokio::spawn(async move {
-            if let Err(_) = tx.send((i, v.clone(), domain_metadata.mx_check().await)).await {
+            if let Err(_) = tx.send((i, v.clone(), domain_metadata.dns_resolvable().await)).await {
                 println!("received dropped");
                 return;
             }
@@ -31,10 +48,43 @@ async fn main() {
 
     drop(tx);
 
+    let mut enumeration_count = 0;
+
     while let Some(i) = rx.recv().await {
         match i.2 {
-            Ok(v) => println!("got: {:?}", v),
+            Ok(v) => {
+                match v.ips {
+                    Some(_) => {
+                        enumeration_count += 1;
+                        println!(
+                            "\n{}\nDomain: {}\n IPs: {:?}",
+                            "Enriched Domain".bold(),
+                            &v.fqdn,
+                            &v.ips
+                        );
+                    },
+                    None => {},
+                }
+            },
             Err(_) => {},
         }
     }
+
+    println!(
+        "\n{}: {}",
+        "Total number of unique domain permutations generated".bold(),
+        domain_permutation_count.to_string().cyan()
+    );
+
+    println!(
+        "{}: {}",
+        "Total number of domains enriched".bold(),
+        enumeration_count.to_string().cyan()
+    );
+
+    println!(
+        "{}: {} seconds",
+        "Execution time".bold(),
+        start_time.elapsed().as_secs()
+    );
 }
